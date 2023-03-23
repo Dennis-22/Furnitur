a75d241d9d12ed5751ff8ba45b9db8b992743ca7
Dennis Jeminal <dennisjeminal8@gmail.com>
2023-03-23 10:56:01 -0400

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const navLists = document.querySelectorAll('[nav-list]')
 const drawerIcon = document.querySelector('[drawer-icon]')
 const mobileNav = document.querySelector('[mobile-nav]')
 const mobileNavClose = document.querySelector('[mobile-nav-close]') 
+const mobileNavLists = document.querySelectorAll('[mobile-nav-list]')
+
+
+navLists.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let moveTo = e.target.getAttribute('move-to')
+        document.querySelector(`[${moveTo}]`).scrollIntoView(true)
+    })
+})
 
 drawerIcon.addEventListener('click', ()=>{
     mobileNav.style.display = 'block'
@@ -10,7 +20,22 @@ mobileNavClose.addEventListener('click', ()=>{
     mobileNav.style.display = 'none'
 })
 
-// window.addEventListener('resize', ()=>{
-//     console.log(window.innerWidth)
-// })
-  
+mobileNavLists.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let moveTo = e.target.getAttribute('move-to')
+        document.querySelector(`[${moveTo}]`).scrollIntoView(true)
+        mobileNav.style.display = 'none'
+    })
+})
+
+// hero animation
+const heroTm = gsap.timeline()
+const heroMainText = document.querySelector('[hero-main-text]')
+const heroSubText = document.querySelector('[hero-sub-text]')
+
+heroTm.to(heroMainText, {opacity:1, scale:1})
+heroTm.to(heroSubText, {opacity:1, y:0})
+
+function handleScrollToSection(section){
+    document.querySelector(`[${section}]`).scrollIntoView(true)
+}
